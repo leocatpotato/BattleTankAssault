60ecd5f1d763a8c8602b2684c2596de139f35885
Leo <78627624+leocatpotato@users.noreply.github.com>
2025-11-28 15:09:44 -0500

    Trees and grass
    add trees, paint ground texture

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+
 public class Shell : MonoBehaviour
 {
-    public float life = 3f; public float damage = 40f; public float blastRadius = 3.5f; public float blastForce = 900f;
-    void Start() { Destroy(gameObject, life); }
+    public float life = 10f;
+    public float damage = 40f;
+    public float blastRadius = 3.5f;
+
+    void Start()
+    {
+        Destroy(gameObject, life);
+    }
+
     void OnCollisionEnter(Collision c)
     {
         foreach (var col in Physics.OverlapSphere(transform.position, blastRadius))
         {
-            if (col.attachedRigidbody)
+            if (col.TryGetComponent<Health>(out var h))
             {
-                col.attachedRigidbody.AddExplosionForce(blastForce, transform.position, blastRadius, 0.5f, ForceMode.Impulse);
+                h.Apply(damage);
             }
-            if (col.TryGetComponent<Health>(out var h)) { h.Apply(damage); }
         }
+
         Destroy(gameObject);
     }
 }
